f3fb39d42f930f0447b2f46f8df46d9523cf5b61
broberts2 <broc.roberts@outlook.com>
2023-03-08 08:46:16 -0700

    changes

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,31 @@
-const typescript = require("@rollup/plugin-typescript");
+const typescript = require("rollup-plugin-ts");
+const commonjs = require("@rollup/plugin-commonjs");
+const nodeResolve = require("@rollup/plugin-node-resolve");
+const nodePolyfills = require("rollup-plugin-node-polyfills");
+const peerDepsExternal = require("rollup-plugin-peer-deps-external");
+const dts = require("rollup-plugin-dts");
+const babel = require("@rollup/plugin-babel").babel;
+const json = require("@rollup/plugin-json");
 const packageJson = require("./package.json");
 module.exports = [
 	{
 		input: "index.ts",
-		output: {
-			file: packageJson.main,
-			format: "cjs",
-		},
-		plugins: [typescript()],
+		output: [
+			{
+				file: packageJson.main,
+				format: "cjs",
+				sourcemap: false,
+			},
+		],
+		plugins: [
+			commonjs({
+				include: "node_modules/**/*",
+			}),
+			nodeResolve(),
+			nodePolyfills(),
+			json(),
+			peerDepsExternal(),
+			typescript(),
+		],
 	},
 ];
