af68fd058d3096265a81bb0cd33d577ed1f54de0
broberts2 <broc.roberts@outlook.com>
2023-07-23 19:20:48 -0600

    changes

--- server/operations/verifyregisteruser.ts
+++ server/operations/verifyregisteruser.ts
@@ -1,45 +1,44 @@
 module.exports =
-	(
-		modules: { [key: string]: any },
-		name: string,
-		transforms: { [key: string]: Function }
-	) =>
-	(io: { [key: string]: any }, socket: { [key: string]: any }) =>
-	async (msg: { [key: string]: any }) => {
-		const R = await modules._models.settings.findOne({ name: "default" });
-		if (!R || !R.userregistration) return;
-		let DSel;
-		try {
-			DSel = JSON.parse(atob(msg.v));
-		} catch (e) {
-			return io.to(socket.id).emit(`servererror`, {
-				code: 401,
-				msg: `Malformed token.`,
-			});
-		}
-		console.log(DSel);
-		const U = await modules._models.user.findOne({
-			_id: DSel.id,
-			_unverified: true,
-		});
-		if (!U)
-			return io.to(socket.id).emit(`servererror`, {
-				code: 400,
-				msg: `Sign-up link invalid or expired.`,
-			});
-		const auth = msg.password === modules.Cryptr.decrypt(DSel.p);
-		if (!auth)
-			return io.to(socket.id).emit(`servererror`, {
-				code: 403,
-				msg: `User password was incorrect.`,
-			});
-		await modules._models.user.updateOne(
-			{ _id: U._id },
-			{ $unset: { _unverified: true } }
-		);
-		io.to(socket.id).emit(`clearurlparameters`);
-		return io.to(socket.id).emit(`serversuccess`, {
-			code: 203,
-			msg: `User registration successful.`,
-		});
-	};
+  (
+    modules: { [key: string]: any },
+    name: string,
+    transforms: { [key: string]: Function }
+  ) =>
+  (io: { [key: string]: any }, socket: { [key: string]: any }) =>
+  async (msg: { [key: string]: any }) => {
+    const R = await modules._models.settings.findOne({ name: "default" });
+    if (!R || !R.userregistration) return;
+    let DSel;
+    try {
+      DSel = JSON.parse(atob(msg.v));
+    } catch (e) {
+      return io.to(socket.id).emit(`servererror`, {
+        code: 401,
+        msg: `Malformed token.`,
+      });
+    }
+    const U = await modules._models.user.findOne({
+      _id: DSel.id,
+      _unverified: true,
+    });
+    if (!U)
+      return io.to(socket.id).emit(`servererror`, {
+        code: 400,
+        msg: `Sign-up link invalid or expired.`,
+      });
+    const auth = msg.password === modules.Cryptr.decrypt(DSel.p);
+    if (!auth)
+      return io.to(socket.id).emit(`servererror`, {
+        code: 403,
+        msg: `User password was incorrect.`,
+      });
+    await modules._models.user.updateOne(
+      { _id: U._id },
+      { $unset: { _unverified: true } }
+    );
+    io.to(socket.id).emit(`clearurlparameters`);
+    return io.to(socket.id).emit(`serversuccess`, {
+      code: 203,
+      msg: `User registration successful.`,
+    });
+  };
--- server/runroutes.ts
+++ server/runroutes.ts
@@ -1,124 +1,137 @@
 module.exports = (modules: any) => async (req: any, res: any) => {
-	try {
-		const accesstype = req.method.toLowerCase();
-		const E = await modules._models.endpoint.findOne({
-			accessurl: req.params.endpoint,
-			accesstype,
-		});
-		if (!E) return res.status(404).send("Endpoint not found.");
-		if (!req || !req.body) return res.status(401).send("Missing request body.");
-		const metaData =
-			typeof req.body.metaData === "string"
-				? JSON.parse(req.body.metaData)
-				: req.body.metaData;
-		const apitoken =
-			(req.query && req.query.apitoken) ||
-			(req.body && (req.body.apitoken || metaData?.apitoken));
-		delete req.query.apitoken;
-		delete req.body.apitoken;
-		if (metaData) {
-			delete metaData.apitoken;
-			req.body.metaData =
-				typeof req.body.metaData === "string"
-					? JSON.stringify(metaData)
-					: metaData;
-		}
-		if (!apitoken || (apitoken && E.apikeyaccess !== apitoken)) {
-			if (!req.body.username || !req.body.password)
-				return res
-					.status(403)
-					.send(
-						"Missing/invalid username, password, or API Token in request query/body."
-					);
-			const U = await modules._models.user.findOne({
-				username: req.body.username,
-			});
-			if (!U) return res.status(401).send("User not found.");
-			if (modules.Cryptr.decrypt(U._password) !== req.body.password)
-				return res
-					.status(403)
-					.send("Invalid username or password in request body.");
-			if (
-				!U.profiles.some((s: string) =>
-					E.profileaccess.find((ss: string) => ss === s.toString())
-				)
-			)
-				return res.status(403).send("Forbidden.");
-		}
-		const preProcess = Object.keys(modules.Integrations).find(
-			(k: string) => modules.Integrations[k][E.accessurl]
-		);
-		let query;
-		if (req.query.query && typeof req.query.query === "string") {
-			try {
-				query = JSON.parse(req.query.query);
-			} catch (e) {
-				console.log(e);
-			}
-		} else if (req.body.query && typeof req.body.query === "object") {
-			try {
-				query = req.body.query;
-			} catch (e) {}
-		}
-		if (accesstype === "get") {
-			const r = query && typeof query === "object" && query._recursive;
-			if (r) delete query._recursive;
-			const records = r
-				? await modules.recursiveLookup(
-						E.recordtype,
-						query && typeof query === "object" ? query : {},
-						{
-							skip: 0,
-						}
-				  )
-				: await modules._models[E.recordtype].find(
-						query && typeof query === "object" ? query : {}
-				  );
-			return res.json(records);
-		} else if (accesstype === "post") {
-			if ((req.body.query && Object.keys(req.body.query)) || E.nonstrict) {
-				if (!req.body.records && !E.nonstrict)
-					return res
-						.status(402)
-						.send("Missing 'records' key in request body for POST.");
-				const _ = E.nonstrict ? req.body : req.body.records;
-				const records = preProcess
-					? await modules.Integrations[preProcess][E.accessurl](_)
-					: _;
-				const results = await modules._models[E.recordtype].insertMany(records);
-				return res.json(results);
-			} else
-				return res
-					.status(402)
-					.send("Non-empty object query required in request body for POST.");
-		} else if (accesstype === "put") {
-			if ((req.body.query && Object.keys(req.body.query)) || E.nonstrict) {
-				if (!req.body.records)
-					return res
-						.status(402)
-						.send("Missing 'records' key in request body for PUT.");
-				const records = await modules._models[E.recordtype].updateMany(
-					E.nonstrict ? {} : req.body.query,
-					E.nonstrict ? req.body : req.body.records
-				);
-				return res.json(records);
-			} else
-				return res
-					.status(402)
-					.send("Non-empty object query required in request body for PUT.");
-		} else if (accesstype === "delete") {
-			if ((req.body.query && Object.keys(req.body.query)) || E.nonstrict) {
-				const records = await modules._models[E.recordtype].deleteMany(
-					E.nonstrict ? {} : req.body.query
-				);
-				return res.json(records);
-			} else
-				return res
-					.status(402)
-					.send("Non-empty object query required in request body for DELETE.");
-		}
-	} catch (e) {
-		console.log(e);
-		return res.status(500).send(e);
-	}
+  try {
+    const accesstype = req.method.toLowerCase();
+    const E = await modules._models.endpoint.findOne({
+      accessurl: req.params.endpoint,
+      accesstype,
+    });
+    if (!E) return res.status(404).send("Endpoint not found.");
+    if (!req || !req.body) return res.status(401).send("Missing request body.");
+    const metaData =
+      typeof req.body.metaData === "string"
+        ? JSON.parse(req.body.metaData)
+        : req.body.metaData;
+    const apitoken =
+      (req.query && req.query.apitoken) ||
+      (req.body && (req.body.apitoken || metaData?.apitoken));
+    delete req.query.apitoken;
+    delete req.body.apitoken;
+    if (metaData) {
+      delete metaData.apitoken;
+      req.body.metaData =
+        typeof req.body.metaData === "string"
+          ? JSON.stringify(metaData)
+          : metaData;
+    }
+    if (!apitoken || (apitoken && E.apikeyaccess !== apitoken)) {
+      if (!req.body.username || !req.body.password)
+        return res
+          .status(403)
+          .send(
+            "Missing/invalid username, password, or API Token in request query/body."
+          );
+      const U = await modules._models.user.findOne({
+        username: req.body.username,
+      });
+      if (!U) return res.status(401).send("User not found.");
+      if (modules.Cryptr.decrypt(U._password) !== req.body.password)
+        return res
+          .status(403)
+          .send("Invalid username or password in request body.");
+      if (
+        !U.profiles.some((s: string) =>
+          E.profileaccess.find((ss: string) => ss === s.toString())
+        )
+      )
+        return res.status(403).send("Forbidden.");
+    }
+    const preProcess = Object.keys(modules.Integrations).find(
+      (k: string) => modules.Integrations[k][E.accessurl]
+    );
+    let query;
+    if (req.query.query && typeof req.query.query === "string") {
+      try {
+        query = JSON.parse(req.query.query);
+      } catch (e) {
+        console.log(e);
+      }
+    } else if (req.body.query && typeof req.body.query === "object") {
+      try {
+        query = req.body.query;
+      } catch (e) {}
+    }
+    if (accesstype === "get") {
+      let skip, limit, sort;
+      const r = query && typeof query === "object" && query._recursive;
+      if (r) delete query._recursive;
+      if (query && typeof query === "object") {
+        skip = query.skip;
+        limit = query.limit;
+        sort = query.sort;
+        delete query.skip;
+        delete query.limit;
+        delete query.sort;
+      }
+      const records = r
+        ? await modules.recursiveLookup(
+            E.recordtype,
+            query && typeof query === "object" ? query : {},
+            {
+              skip,
+              limit,
+              sort,
+            }
+          )
+        : await modules._models[E.recordtype]
+            .find(query && typeof query === "object" ? query : {})
+            .skip(skip)
+            .limit(limit)
+            .sort(sort);
+      return res.json(records);
+    } else if (accesstype === "post") {
+      if ((req.body.query && Object.keys(req.body.query)) || E.nonstrict) {
+        if (!req.body.records && !E.nonstrict)
+          return res
+            .status(402)
+            .send("Missing 'records' key in request body for POST.");
+        const _ = E.nonstrict ? req.body : req.body.records;
+        const records = preProcess
+          ? await modules.Integrations[preProcess][E.accessurl](_)
+          : _;
+        const results = await modules._models[E.recordtype].insertMany(records);
+        return res.json(results);
+      } else
+        return res
+          .status(402)
+          .send("Non-empty object query required in request body for POST.");
+    } else if (accesstype === "put") {
+      if ((req.body.query && Object.keys(req.body.query)) || E.nonstrict) {
+        if (!req.body.records)
+          return res
+            .status(402)
+            .send("Missing 'records' key in request body for PUT.");
+        const records = await modules._models[E.recordtype].updateMany(
+          E.nonstrict ? {} : req.body.query,
+          E.nonstrict ? req.body : req.body.records
+        );
+        return res.json(records);
+      } else
+        return res
+          .status(402)
+          .send("Non-empty object query required in request body for PUT.");
+    } else if (accesstype === "delete") {
+      if ((req.body.query && Object.keys(req.body.query)) || E.nonstrict) {
+        const records = await modules._models[E.recordtype].deleteMany(
+          E.nonstrict ? {} : req.body.query
+        );
+        return res.json(records);
+      } else
+        return res
+          .status(402)
+          .send("Non-empty object query required in request body for DELETE.");
+    }
+  } catch (e) {
+    console.log(e);
+    return res.status(500).send(e);
+  }
 };
