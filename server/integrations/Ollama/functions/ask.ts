853286476d2a29f338d3fd295ad0cc1fd673456f
broberts2 <broc.roberts@outlook.com>
2024-03-03 04:54:39 -0700

    pre-publish commit

--- server/integrations/Ollama/functions/ask.ts
+++ server/integrations/Ollama/functions/ask.ts
@@ -1,22 +1,26 @@
 module.exports =
   (modules: { [key: string]: any }) =>
   async (Settings: any, prompt: string) => {
-    const res = await modules
-      .fetch(`http://127.0.0.1:11434/api/generate`, {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          stream: false,
-          model: "ivy",
-          prompt,
-        }),
-      })
-      .then((res: any) => res.json())
-      .then((res: any) => {
-        res.response = res?.response ? res.response.trim() : "";
-        return res;
-      });
-    return res;
+    try {
+      const res = await modules
+        .fetch(`http://127.0.0.1:11434/api/generate`, {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            stream: false,
+            model: "ivy",
+            prompt,
+          }),
+        })
+        .then((res: any) => res.json())
+        .then((res: any) => {
+          res.response = res?.response ? res.response.trim() : "";
+          return res;
+        });
+      return res;
+    } catch (e) {
+      return "[Failed to connect with ollama]";
+    }
   };
